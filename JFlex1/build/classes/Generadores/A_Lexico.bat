97e7f36719982258058ab0e98f921162b7b19f13
Rguerram5 <128444073+Rguerram5@users.noreply.github.com>
2024-04-06 20:06:24 -0600

    Add files via upload

---
+++ JFlex1/build/classes/Generadores/A_Lexico.bat
@@ -0,0 +1,6 @@
+SET JAVA_HOME="C:\Program Files\Java\jdk-21\bin"
+SET PATH=%JAVA_HOME%;%PATH%
+SET CLASSPATH=%JAVA_HOME%;
+cd C:\Users\HP\Desktop\Uni 2023\Pagos U\Semestre 7\Programación\Tareas\JFlex1\src\Analizadores
+java -jar C:\Users\HP\Desktop\Fuentes\jflex-1.6.1\jflex-1.6.1\lib\jflex-1.6.1.jar A_Lexico.jflex
+pause
